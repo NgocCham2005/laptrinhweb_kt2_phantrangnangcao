bed29efcf4189f65a59cd249c8de77b844d99ac1
NgocCham2005 <nguyenngoccham091105@gmail.com>
2026-04-17 21:20:09 +0700

    push code ver1

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+// Đường dẫn (Đổi thành đường dẫn nơi lưu folder của mình)
+define('BASE_URL', 'http://localhost/LAPTRINHWEB_KT2_PHANTRANGNANGCAO/');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'ql_trangsuc');
+define('DB_USER', 'root');
+define('DB_PASS', '');
---
+++ core/Database.php
@@ -0,0 +1,27 @@
+<?php
+class Database {
+    private static $instance = null;
+    private $conn;
+
+    private function __construct() {
+        try {
+            $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
+            $this->conn = new PDO($dsn, DB_USER, DB_PASS, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance(): self {
+        if (!self::$instance) self::$instance = new self();
+        return self::$instance;
+    }
+
+    public function getConnection(): PDO {
+        return $this->conn;
+    }
+}
+?>

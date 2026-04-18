b9767cb93d51c07f065ab6fd9ae15b4f82521350
ptn481 <phuongthaonguyen481@gmail.com>
2026-04-18 17:32:22 +0700

    submit 18.04 v1

---
+++ feature/ui-ux-views/app/views/layouts/footer.php
@@ -0,0 +1,9 @@
+</main></div> <footer class="site-footer">
+        <div class="container footer-content">
+            <p>&copy; <?php echo date('Y'); ?> - BÀI KIỂM TRA 2 MÔN LẬP TRÌNH WEB PHP</p>
+        </div>
+    </footer>
+
+    <script src="/public/js/app.js"></script>
+</body>
+</html>

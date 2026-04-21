c8157c26cad5a19a25f9eb4a68aa05f5fb7bc52b
ptn481 <phuongthaonguyen481@gmail.com>
2026-04-21 23:07:02 +0700

    Xoa thu muc feature/ui-ux-views tren develop

--- feature/ui-ux-views/app/views/layouts/footer.php
+++
@@ -1,9 +0,0 @@
-</main></div> <footer class="site-footer">
-        <div class="container footer-content">
-            <p>&copy; <?php echo date('Y'); ?> - BÀI KIỂM TRA 2 MÔN LẬP TRÌNH WEB PHP</p>
-        </div>
-    </footer>
-
-    <script src="/public/js/app.js"></script>
-</body>
-</html>

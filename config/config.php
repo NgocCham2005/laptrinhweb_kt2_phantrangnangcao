2f27c63735282dce6aaf3f233dd986930011ed4d
ptn481 <phuongthaonguyen481@gmail.com>
2026-04-24 07:37:07 +0700

    Delete config directory

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Đường dẫn (Đổi thành đường dẫn nơi lưu folder của mình)
-define('BASE_URL', 'http://localhost/KT2/KT2 - merge test/public');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'ql_trangsuc');
-define('DB_USER', 'root');
-define('DB_PASS', '');

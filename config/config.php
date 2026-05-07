4e04b62f46a935c215d5f9a806da78fe4293f86b
ptn481 <phuongthaonguyen481@gmail.com>
2026-05-07 11:30:17 +0700

    thay đổi đường dẫn

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 // Đường dẫn (Đổi thành đường dẫn nơi lưu folder của mình)
-define('BASE_URL', 'http://localhost/KT2/laptrinhweb_kt2_phantrangnangcao');
+define('BASE_URL', 'http://localhost/laptrinhweb_kt2_phantrangnangcao');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'qly_trangsuc');
 define('DB_USER', 'root');

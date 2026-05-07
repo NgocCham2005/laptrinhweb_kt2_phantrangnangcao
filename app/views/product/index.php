0185841dd94ae2b88e0471fb14d03a3e7126c7d3
Thu Trang <saira87205@gmail.com>
2026-05-07 11:25:26 +0700

    edit url

--- app/views/product/index.php
+++ app/views/product/index.php
@@ -67,4 +67,4 @@
         </div>
     </div>
 </div>
-<script src="/webproject/laptrinhweb_kt2_phantrangnangcao/public/js/app.js"></script>
+<script src="/laptrinhweb_kt2_phantrangnangcao/public/js/app.js"></script>
--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 // Đường dẫn (Đổi thành đường dẫn nơi lưu folder của mình)
-define('BASE_URL', 'http://localhost/webproject/laptrinhweb_kt2_phantrangnangcao/public');
+define('BASE_URL', 'http://localhost/laptrinhweb_kt2_phantrangnangcao/public');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'qly_trangsuc');
 define('DB_USER', 'root');

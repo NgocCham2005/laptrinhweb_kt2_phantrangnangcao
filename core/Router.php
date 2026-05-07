2e6bda009bce37b99b7ec9fd2d2b1c89ce27cd23
NgocCham2005 <nguyenngoccham091105@gmail.com>
2026-05-07 16:51:32 +0700

    Update core-system

--- core/Router.php
+++ core/Router.php
@@ -1,10 +1,10 @@
 <?php
-class Router {
+class Router { //Đọc URL và cắt URL thành các phần để App có thể sử dụng
     public static function parseUrl(): array {
         if (isset($_GET["url"])) {
-            $url = rtrim($_GET["url"], "/");
+            $url = rtrim($_GET["url"], "/");// Loại bỏ dấu / ở cuối URL nếu có
             $url = filter_var($url, FILTER_SANITIZE_URL);
-            return explode("/", $url);
+            return explode("/", $url);// Cắt URL thành mảng dựa trên dấu / 
         }
         return []; // Trả về mảng rỗng nếu ở trang chủ
     }
